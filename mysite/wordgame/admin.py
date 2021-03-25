073a864b4b63d4631628795936a8b58e6a2734f0
benjstrubel <bstrubel@gmail.com>
2021-03-24 22:49:21 -0400

    working

--- mysite/wordgame/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Blurb
-from .models import ScoreVector
-
-# Register your models here.
-admin.site.register(Blurb)
-admin.site.register(ScoreVector)
